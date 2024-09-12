c3b83a13b0f8b50fc12a1da058cd33472012790e
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2024-09-12 11:21:26 +0200

    directions

--- main4.py
+++ main4.py
@@ -155,6 +155,48 @@ game_board = [
 solution = sokoban_solver_bfs(game_board)
 
 if solution:
+    move = direction_to_string(solution.pop(0))
+
+    if angle == 0:
+        if move == 'Up'
+            # Go forward
+        if move == 'Down'
+            # Go backwards
+        if move == 'Left'
+            # Go left
+        if move == 'Right'
+            # Go right
+    if angle == 90:
+        if move == 'Up'
+            # Go left
+        if move == 'Down'
+            # Go right
+        if move == 'Left'
+            # Go backwards
+        if move == 'Right'
+            # Go forward
+    if angle == 180:  
+        if move == 'Up'
+            # Go backwards
+        if move == 'Down'
+            # Go forward
+        if move == 'Left'
+            # Go right
+        if move == 'Right'
+            # Go left
+    if angle == 270:
+        if move == 'Up'
+            # Go right
+        if move == 'Down'
+            # Go left
+        if move == 'Left'
+            # Go forward
+        if move == 'Right'
+            # Go backwards
+
+
+
+
     print("Solved! Moves:", [direction_to_string(move) for move in solution])
 else:
     print("Unsolvable")
